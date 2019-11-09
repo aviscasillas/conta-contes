e12ccfd526a82b4ca58be15ad4cb5ea181e75554
Albert Viscasillas <aviscasillas@gmail.com>
2019-11-09 23:44:25 +0100

    First function

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.processTale = functions.https.onRequest((request, response) => {
+  var responseBody = { name: 'el-secret-de-la-lluna-ivet-mataro-celia-albert.mp3' };
+  response.send(responseBody);
+});
